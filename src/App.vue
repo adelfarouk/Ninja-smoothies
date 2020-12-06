d16297a6a6ef7be007d408eaddd646e899afd718
adel <adelfarouk77@gmail.com>
2020-12-06 22:46:17 +0200

    before work with firebase

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <Navbar />
-    <!-- <router-view/> -->
+    <router-view/>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
+import Index from '@/components/Index'
 export default {
   name: 'App',
   components: {
     Navbar
   }
 }
+
 </script>
+
 
 <style>
 
